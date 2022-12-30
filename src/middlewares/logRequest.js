404ab47528339eec93109ee571b1b928c06efd68
Konstantin Kozokar <konstantin.kozokar@gmail.com>
2022-12-30 18:55:25 +0100

    push

--- src/middlewares/logRequest.js
+++ src/middlewares/logRequest.js
@@ -2,6 +2,6 @@ import { LogModel } from "../model/log.model";
 
 export function logRequest(req, res, next) {
   const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
-  LogModel.create({ body: req.body, ip });
+  LogModel.create({ body: req.body, headers: req.headers });
   next();
 }
